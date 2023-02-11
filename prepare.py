792e20b4201d2ac8be54f006123615b96a8a2721
vinhtran <tranquocvinh2611@gmail.com>
2023-02-11 22:26:26 +0700

    creat dataloader

--- prepare.py
+++ prepare.py
@@ -4,44 +4,13 @@ from os.path import exists
 import spacy
 import torchtext.datasets as datasets
 from torchtext.vocab import build_vocab_from_iterator
+from torch.nn.functional import pad
+from torchtext.data.functional import to_map_style_dataset
+from torch.utils.data import DistributedSampler, DataLoader
 
 
-from utils import subsequent_mask
+from utils import Batch, tokenize, yield_tokens
 
-# Batches and Masking
-class Batch:
-    """
-    Object for holding a batch of data with mask during training.
-    """
-
-    def __init__(self, src, tgt=None, pad=2):
-        """
-        Args:
-            src: (batch_size, src_vocab)
-            tgt: (batch_size, tgt_vocab)
-            2 = <blank>
-        """
-        self.src = src
-        # (batch_size, 1, src_vocab)
-        self.src_mask = (src != pad).unsqueeze(-2)
-
-        if tgt is not None:
-            self.tgt = tgt[:, :-1]
-            self.tgt_y = tgt[:, 1:]
-            self.tgt_mask = self.make_std_mask(self.tgt, pad)
-            self.ntokens = (self.tgt_y != pad).data.sum()
-
-    @staticmethod
-    def make_std_mask(tgt, pad):
-        """
-        Create a mask to hide padding and future words.
-        """
-        
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(
-            tgt_mask.data
-        )
-        return tgt_mask
 
 def data_gen(V, batch_size, nbatches):
     """
@@ -57,8 +26,6 @@ def data_gen(V, batch_size, nbatches):
 
 # Load spacy tokenizer models, download them if they haven't been
 # downloaded already
-
-
 def load_tokenizers():
     try:
         spacy_de = spacy.load("de_core_news_sm")
@@ -75,17 +42,12 @@ def load_tokenizers():
     return spacy_de, spacy_en
 
 
-def tokenize(text, tokenizer):
-    return [tok.text for tok in tokenizer.tokenizer(text)]
-
-def yield_tokens(data_iter, tokenizer, index):
-    """
-    index = 0 for "de", index = 1 for "en"
-    """
-    for from_to_tuple in data_iter:
-        yield tokenizer(from_to_tuple[index])
-
 def build_vocabulary(spacy_de, spacy_en):
+    """
+    Vocabulary sizes:
+    German is 8315
+    English is 6384
+    """
     def tokenize_de(text):
         return tokenize(text, spacy_de)
 
@@ -123,3 +85,125 @@ def load_vocab(spacy_de, spacy_en):
     print(len(vocab_src))
     print(len(vocab_tgt))
     return vocab_src, vocab_tgt
+
+
+def collate_batch(
+    batch,
+    src_pipeline,
+    tgt_pipeline,
+    src_vocab,
+    tgt_vocab,
+    device,
+    max_padding=128,
+    pad_id=2,
+):
+    bs_id = torch.tensor([0], device=device)  # <s> token id
+    eos_id = torch.tensor([1], device=device)  # </s> token id
+    src_list, tgt_list = [], []
+    for (_src, _tgt) in batch:
+        processed_src = torch.cat(
+            [
+                bs_id,
+                torch.tensor(
+                    src_vocab(src_pipeline(_src)),
+                    dtype=torch.int64,
+                    device=device,
+                ),
+                eos_id,
+            ],
+            0,
+        )
+        processed_tgt = torch.cat(
+            [
+                bs_id,
+                torch.tensor(
+                    tgt_vocab(tgt_pipeline(_tgt)),
+                    dtype=torch.int64,
+                    device=device,
+                ),
+                eos_id,
+            ],
+            0,
+        )
+        src_list.append(
+            # warning - overwrites values for negative values of padding - len
+            pad(
+                processed_src,
+                (
+                    0,
+                    max_padding - len(processed_src),
+                ),
+                value=pad_id,
+            )
+        )
+        tgt_list.append(
+            pad(
+                processed_tgt,
+                (0, max_padding - len(processed_tgt)),
+                value=pad_id,
+            )
+        )
+
+    src = torch.stack(src_list)
+    tgt = torch.stack(tgt_list)
+    return (src, tgt)
+
+
+def create_dataloaders(
+    device,
+    vocab_src,
+    vocab_tgt,
+    spacy_de,
+    spacy_en,
+    batch_size=12000,
+    max_padding=128,
+    is_distributed=True,
+):
+    def tokenize_de(text):
+        return tokenize(text, spacy_de)
+
+    def tokenize_en(text):
+        return tokenize(text, spacy_en)
+
+    def collate_fn(batch):
+        return collate_batch(
+            batch,
+            tokenize_de,
+            tokenize_en,
+            vocab_src,
+            vocab_tgt,
+            device,
+            max_padding=max_padding,
+            pad_id=vocab_src.get_stoi()["<blank>"],
+        )
+
+    train_iter, valid_iter, test_iter = datasets.Multi30k(
+        language_pair=("de", "en")
+    )
+
+    train_iter_map = to_map_style_dataset(
+        train_iter
+    )  # DistributedSampler needs a dataset len()
+    train_sampler = (
+        DistributedSampler(train_iter_map) if is_distributed else None
+    )
+    valid_iter_map = to_map_style_dataset(valid_iter)
+    valid_sampler = (
+        DistributedSampler(valid_iter_map) if is_distributed else None
+    )
+
+    train_dataloader = DataLoader(
+        train_iter_map,
+        batch_size=batch_size,
+        shuffle=(train_sampler is None),
+        sampler=train_sampler,
+        collate_fn=collate_fn,
+    )
+    valid_dataloader = DataLoader(
+        valid_iter_map,
+        batch_size=batch_size,
+        shuffle=(valid_sampler is None),
+        sampler=valid_sampler,
+        collate_fn=collate_fn,
+    )
+    return train_dataloader, valid_dataloader
--- utils.py
+++ utils.py
@@ -17,4 +17,47 @@ def subsequent_mask(size):
     )
     return subsequent_mask == 0
 
+# Batches and Masking
+class Batch:
+    """
+    Object for holding a batch of data with mask during training.
+    """
 
+    def __init__(self, src, tgt=None, pad=2):
+        """
+        Args:
+            src: (batch_size, src_vocab)
+            tgt: (batch_size, tgt_vocab)
+            2 = <blank>
+        """
+        self.src = src
+        # (batch_size, 1, src_vocab)
+        self.src_mask = (src != pad).unsqueeze(-2)
+
+        if tgt is not None:
+            self.tgt = tgt[:, :-1]
+            self.tgt_y = tgt[:, 1:]
+            self.tgt_mask = self.make_std_mask(self.tgt, pad)
+            self.ntokens = (self.tgt_y != pad).data.sum()
+
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        """
+        Create a mask to hide padding and future words.
+        """
+        
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(
+            tgt_mask.data
+        )
+        return tgt_mask
+
+def tokenize(text, tokenizer):
+    return [tok.text for tok in tokenizer.tokenizer(text)]
+
+def yield_tokens(data_iter, tokenizer, index):
+    """
+    index = 0 for "de", index = 1 for "en"
+    """
+    for from_to_tuple in data_iter:
+        yield tokenizer(from_to_tuple[index])
