0a4ae0c95ecc65cb91b63ab5b86e2979b27ad5b8
vinhtran <tranquocvinh2611@gmail.com>
2023-02-11 15:04:27 +0700

    add loss compute

---
+++ loss.py
@@ -0,0 +1,18 @@
+import torch
+
+class SimpleLossCompute:
+    "A simple loss compute and train function."
+
+    def __init__(self, generator, criterion):
+        self.generator = generator
+        self.criterion = criterion
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        sloss = (
+            self.criterion(
+                x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1)
+            )
+            / norm
+        )
+        return sloss.data * norm, sloss
